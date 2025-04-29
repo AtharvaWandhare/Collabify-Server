586800624604979bb7965de7b2d2c03b40c1af7b
AtharvaWandhare <atharvawandhare@gmail.com>
2025-04-29 10:59:02 +0530

    feat: implement server setup with HTTPS and socket.io integration

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import https from 'https';
+import { Server } from 'socket.io';
+import fs from 'fs';
+import passport from 'passport';
+import session from 'cookie-session';
+
+const app = express();
+
+const key = fs.readFileSync('../secureKeys/key.pem')
+const cert = fs.readFileSync('../secureKeys/cert.pem')
+
+const server = https.createServer({ key, cert }, app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CORS_ORIGIN,
+        methods: ['GET', 'POST'],
+        credentials: true
+    },
+})
+
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.json({ limit: '16kb' }));
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(session({
+    secret: process.env.Client_Secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+    keys: [process.env.Client_Secret]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Import Routes
+import userRoutes from './routes/user.routes.js';
+import userProfileRoutes from './routes/userProfile.routes.js';
+import userNotifications from './routes/userNotifications.routes.js';
+import userSettings from './routes/userSettings.routes.js';
+import GoogleAuth from './routes/auth.routes.js';
+
+// Use Routes
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/userProfile', userProfileRoutes);
+app.use('/api/v1/userNotifications', userNotifications);
+app.use('/api/v1/userSettings', userSettings);
+app.use('/auth', GoogleAuth);
+
+export { app, io, server };
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+// dotenv.configDotenv({ path: '../../.env' })
+
+import { app, io, server } from './app.js';
+import connectDB from './config/db.connect.js';
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+    .then(() => {
+
+        app.get('/', (req, res) => {
+            res.status(200).send('Server is running');
+        })
+
+        io.on('connection', (socket) => {
+            console.log('Socket connected: ', socket.id);
+
+            socket.on('disconnect', () => {
+                console.log('Socket disconnected: ', socket.id);
+            })
+        })
+
+        server.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    })
+    .catch(err => {
+        console.log(`MongoDB connection Error: ${err}`);
+    });
